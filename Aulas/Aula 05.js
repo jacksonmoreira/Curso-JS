0d2008375feea06015def085881811b41744ff20
jacksonmoreira <jacksonmoreiraneto@gmail.com>
2022-06-19 10:47:55 -0300

    1.0

---
+++ Aulas/Aula 05.js
@@ -0,0 +1,6 @@
+//Esse aqui é para um comentário com apenas uma linha!
+/*Esse aqui é para comentários com mais de uma linha. O primeiro é mais fácil, mas irei optar por este pois é mais universal por assim dizer!*/
+
+var n1 = 100
+var n2 = 250
+n1 + n2
